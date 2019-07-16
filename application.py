9c4c6faac140cfe2d755903436a0d030416a9f25
choisteph <stchoi@umich.edu>
2019-07-16 11:07:01 -0400

    deleted commented-out code (unused)

--- application.py
+++ application.py
@@ -89,31 +89,6 @@ def generate_report_given_name():
 
     return render_template("county_src_report.html", data=data, T0=T0, T1=T1)
 
-
-# @application.route('/<string:county>/<string:src>/', defaults={'T0':14, 'T1':None}, methods=['GET'])
-# @application.route('/<string:county>/<string:src>/<int:T0>', defaults={'T1':None}, methods=['GET'])
-# @application.route('/<string:county>/<string:src>/<int:T0>/<int:T1>', methods=['GET'])
-# def one_page_report(county, src, T0, T1):
-#     county = county.title()
-#     src = src.upper()
-#     create_county_files(county, src, T0, T1)
-#     if county in cities:
-#         folder = 'cities'
-#         county_flag = ''
-#     else:
-#         folder = 'counties'
-#         county_flag = 'County'
-#     data = {
-#         'county': county,
-#         'src': src,
-#         'county_flag': county_flag,
-#         'titlename': src_dict[src],
-#         'f_geojson': f'geojson/{folder}/{county}.geojson',
-#         'center': center_dict[county]['center'],
-#         'zoom': center_dict[county].get('zoom',10),
-#     }
-#     return render_template("county_src_report.html", data=data)
-
 #%% Run Flask app
 # python application.py
 if __name__ == '__main__':
